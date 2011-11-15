92d520f411e7b010c7600022baaf012338102498
dangodfrey <c.daniel.godfrey@gmail.com>
2011-11-15 17:24:53 -0500

    changing inports

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+# Auto-require default libraries and those for the current Rails environment. 
+Bundler.require :default, Rails.env
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
