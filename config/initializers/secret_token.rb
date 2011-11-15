b62a3213d1f874b8640bc12022deeb35399e162f
dangodfrey <c.daniel.godfrey@gmail.com>
2011-11-15 17:06:58 -0500

    master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaKeyPress::Application.config.secret_token = '80293f22f942f0ede1942a0d085599b7b505641123d2b15a3fc840a16851700ca4577c5b83040f225e71c8c25e3480c5cddee5393a0fd667399c8449f725add1'
